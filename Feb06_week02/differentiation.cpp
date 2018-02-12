9101a5fcadccf16a21bb0389e5ead9c018868a4d
Deepak Agarwal <deepak@dhcp-rhodes-2084.redrover.cornell.edu>
2018-02-11 19:55:01 -0500

    table pdf

--- Feb06_week02/differentiation.cpp
+++ Feb06_week02/differentiation.cpp
@@ -29,12 +29,12 @@ int main()
     double f_x = (fx(x+h) - fx(x)) / h;
     double f_x_off = (fx_offset(x+h) - fx_offset(x)) / h;
 
-    double f_xx = (fx(x+h) - fx(x-h)) / 2*h;
-    double f_xx_off = (fx_offset(x+h) - fx_offset(x-h)) / 2*h;
+    double f_xx = (fx(x+h) - fx(x-h)) / (2*h);
+    double f_xx_off = (fx_offset(x+h) - fx_offset(x-h)) / (2*h);
     cout << "f_x is " << f_x << " for h = " << h << endl;
     cout << "f_xx is " << f_xx << " for h = " << h << endl;
     cout << "f_x_off is " << f_x_off << " for h = " << h << endl;
-    cout << "f_xx_off is " << f_xx_off << " for h = " << h << endl;
+    cout << "f_xx_off is " << f_xx_off << " for h = " << h << endl << endl;
     h = h/10;
   }
 
